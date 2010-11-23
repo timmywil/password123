019d227ccecbbefc6bfdb1d607afca8ce2bc6e91
Timmy Willison <tim.willison@thisismedium.com>
2010-11-23 11:28:33 -0500

    Added Closure comments, common password field attribute support when replacing fields, and do not put placeholder value in hidden field anymore (so it will not get sent on submit

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-*Version: 1.2, Last updated: 9/28/2010*
+*Version: 1.3, Last updated: 11/23/2010*
  
 Demo         - <a href="http://timmywillison.com/samples/password123/">http://timmywillison.com/samples/password123/</a><br/>
 GitHub       - <a href="http://github.com/timmywil/password123">http://github.com/timmywil/password123</a><br/>
-Source       - <a href="http://github.com/timmywil/password123/raw/master/jquery.password123.js">http://github.com/timmywil/password123/raw/master/jquery.password123.js (12.4kb)</a><br/>
-(Minified)   - <a href="http://github.com/timmywil/password123/raw/master/jquery.password123.min.js">http://github.com/timmywil/password123/raw/master/jquery.password123.min.js (3.5kb)</a><br/>
+Source       - <a href="http://github.com/timmywil/password123/raw/master/jquery.password123.js">http://github.com/timmywil/password123/raw/master/jquery.password123.js (13.7kb)</a><br/>
+(Minified)   - <a href="http://github.com/timmywil/password123/raw/master/jquery.password123.min.js">http://github.com/timmywil/password123/raw/master/jquery.password123.min.js (3.6kb)</a><br/>
 
 License
 
@@ -15,12 +15,14 @@ Support and Testing
 
 Versions of jQuery and browsers this was tested on.
 
-jQuery Versions - 1.3.0-1.4.2<br/>
+jQuery Versions - 1.3.0-1.4.4<br/>
 Browsers Tested - Internet Explorer 6-8, Firefox 2-3.7, Safari 3-5,<br/>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Chrome 4-5, Opera 9.6-10.5.
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Chrome 4-6, Opera 9.6-10.5.
 
 Release History
 
+1.3   - (11/23/2010) Added Google Closure Compiler comments, common password field attribute<br/>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;support when replacing fields, and no longer sends placeholder value when submitting the form.<br/>
 1.2   - (9/28/2010) Placeholders changed to only work with HTML5 placeholder attribute,<br/>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'value' attribute now reserved for actual values<br/>
 1.1   - (7/5/2010) Add Placeholder functionality<br/>
--- jquery.password123.js
+++ jquery.password123.js
@@ -1,17 +1,17 @@
-/*!
-* jQuery password123: iPhone Style Passwords Plugin - v1.2 - 10/5/2010
-* http://timmywillison.com/samples/password123/
-* Copyright (c) 2010 timmy willison
-* Dual licensed under the MIT and GPL licences.
-* http://timmywillison.com/licence/
+/**
+*   @preserve jQuery password123: iPhone Style Passwords Plugin - v1.3 - 11/23/2010
+*   http://timmywillison.com/samples/password123/
+*   Copyright (c) 2010 timmy willison
+*   Dual licensed under the MIT and GPL licences.
+*   http://timmywillison.com/licence/
 */
 
-// *Version: 1.2, Last updated: 10/5/2010*
+// *Version: 1.3, Last updated: 11/23/2010*
 // 
 // Demo         - http://timmywillison.com/samples/password123/
 // GitHub       - http://github.com/timmywil/password123
-// Source       - http://github.com/timmywil/password123/raw/master/jquery.password123.js (12.5kb)
-// (Minified)   - http://github.com/timmywil/password123/raw/master/jquery.password123.min.js (3.5kb)
+// Source       - http://github.com/timmywil/password123/raw/master/jquery.password123.js (13.7kb)
+// (Minified)   - http://github.com/timmywil/password123/raw/master/jquery.password123.min.js (3.6kb)
 // 
 // License
 // 
@@ -23,23 +23,26 @@
 // 
 // Versions of jQuery and browsers this was tested on.
 // 
-// jQuery Versions - 1.3.0-1.4.2
+// jQuery Versions - 1.3.0-1.4.4
 // Browsers Tested - Internet Explorer 6-8, Firefox 2-3.7, Safari 3-5,
-//                   Chrome 4-5, Opera 9.6-10.5.
+//                   Chrome 4-6, Opera 9.6-10.5.
 // 
 // Release History
 // 
+// 1.3   - (11/23/2010) Added Google Closure Compiler comments, common password field attribute
+//              support when replacing fields, and no longer sends placeholder value when submitting the form.
 // 1.2   - (9/28/2010) Placeholders changed to only work with HTML5 placeholder attribute, 
-//            'value' attribute now reserved for actual values
+//              'value' attribute now reserved for actual values
 // 1.1   - (7/5/2010) Add Placeholder functionality
 // 1.0   - (7/4/2010) Initial release
 //
-// See README for usage and placeholder explanation (it's easy)
+// See README for usage and placeholder explanation
 
-(function ($, window, document, undefined) {
+;(function ($, window, undefined) {
 
     // Method / object references.
-    var last,
+    var document = window.document,
+        last,
         opts = {
         
             // You can use any html character code or
@@ -77,25 +80,29 @@
             $(this).unbind('.textchange');
         }, 
         handler: function (event) {
-            $.event.special.textchange.triggerIfChanged($(this));
+            $.event.special.textchange.triggerIfChanged( $(this) );
         }, 
         delayedHandler: function (event) {
             var $element = $(this);
+            
             setTimeout(function () {
-                $.event.special.textchange.triggerIfChanged($element);
+                $.event.special.textchange.triggerIfChanged( $element );
             }, 25);
         }, 
         triggerIfChanged: function ($element) {
             var v = $element.val(),
                 encodedCharacter = $('<div>'+opts.character+'</div>').text();
+            
             if ($element.val() !== $element.data('lastValue')) {
+                
                 // Check if something larger than one letter was pasted into the field
                 if (v.length > 1 && v.indexOf(encodedCharacter) === -1) {
+                    
                     // If so, we need to save it before it disappears,
                     // but continue with triggering the field change
                     // by taking the last letter off
                     v = v.substr(0, v.length-1);
-                    $element.data('value', v).prev('input').val(v);
+                    $element.data('value', v).prev('input').val( v );
                 }
                 $element.trigger('textchange', $element.data('lastValue'));
                 $element.data('lastValue', $element.val());
@@ -108,19 +115,23 @@
     };
     if ($.attrFn) $.attrFn['textchange'] = true;
 
-    // Gets the current cursor position in a textfield
-    // to determine where to delete/update a character.
-    function getCursorPosition(field) {
-        if (field != null) {
+    /**
+    *   Gets the current cursor position in a textfield
+    *   to determine where to delete/update a character.
+    *   @param {Element} field The raw field to get the cursor pos from.
+    *   @return {Number} The index for the position of the cursor. 
+    */
+    function getCursorPosition( field ) {
+        if ( field != null ) {
             // IE
-            if (document.selection) {
+            if ( document.selection ) {
                 field.focus();
                 var sel = document.selection.createRange();
-                sel.moveStart('character', -field.value.length);
+                sel.moveStart( 'character', -field.value.length );
                 return sel.text.length;
             }
             // Others
-            else if (field.selectionStart || field.selectionStart == '0') {
+            else if ( field.selectionStart || field.selectionStart == '0' ) {
                 return field.selectionStart;
             }
         }
@@ -128,20 +139,24 @@
         return -1;
     }
     
-    // Sets the cursor position to a previous state
-    // if the field was updated somewhere in the middle.
-    function setCursorPosition(field, pos) {
-        if (field != null) {
+    /**
+    *   Sets the cursor position to a previous state
+    *   if the field was updated somewhere in the middle.
+    *   @param {Element} field The raw field.
+    *   @param {Number} pos The position to set to.
+    */
+    function setCursorPosition( field, pos ) {
+        if ( field != null ) {
             // IE
-            if (field.createTextRange) {
+            if ( field.createTextRange ) {
                 var range = field.createTextRange();
                 range.move('character', pos);
                 range.select();
             }
             // Others
-            else if (field.selectionStart) {
+            else if ( field.selectionStart ) {
                 field.focus();
-                field.setSelectionRange(pos, pos);
+                field.setSelectionRange( pos, pos );
             }
             // Something's wrong. Just focus on it.
             else
@@ -149,48 +164,66 @@
         }
     }
     
-    // Replaces the password field with a hidden field
-    // and adds the new visible text field.
-    // Returns the new text fields.
+    /**
+    *   Replaces the password field with a hidden field
+    *   and adds the new visible text field.
+    *   @param {object} $f All of the jQuery fields this plugin is called with.
+    *   @return {object} The new text fields.
+    *   @private
+    */
     function replaceFields( $f ) {
         var fields = [];
-        $f.each(function ( i, item ) {
-
+        $f.each(function (i, item) {
             var $field   = $(item),
-                field_id = opts.prefix + i,
                 place    = $field.attr('placeholder') || undefined,
-                value    = $field.attr('value') || (opts.placeholder ? place || '' : '');
+                field_id = opts.prefix + i,
+                value    = $field.attr('value') || ( opts.placeholder ? place || '' : '' ),
+                classes  = opts.placeholder && place !== undefined && (value === place || value === '') ? $field.attr('class') + ' ' + opts.placeholderClass : $field.attr('class');
 
             // The main field
             $('<input type="text"/>').attr({
-                'class': opts.placeholder && place !== undefined && (value === place || value === '') ? $field.attr('class') + ' ' + opts.placeholderClass : $field.attr('class'),
+                'class': classes,
                 'id': field_id,
                 'value': value,
+                'size': $field.attr('size'),
+                'tabindex': $field.attr('tabindex'),
+                'readonly': $field.attr('readonly'),
+                'disabled': $field.attr('disabled'),
+                'maxlength': $field.attr('maxlength'),
                 'placeholder': opts.placeholder ? undefined : place
-            }).insertAfter($field)
-                // If value was set (tho rare on password fields), fill it in correctly
-                .data('value', $field.attr('value') || '')
-                // Add placeholder data directly to the element
-                .data('placeholder', place);
+            }).insertAfter( $field )
+                // If value was set, fill it in correctly
+                .data({
+                    'value': $field.attr('value') || '',
+                    // Add placeholder data directly to the element
+                    'placeholder': place,
+                    // Add hidden field value to data for placeholder comparison
+                    // This to avoid sending the placeholder value when submitting the form
+                    'newVal': value
+                });
 
-            fields.push(document.getElementById(field_id));
+            fields.push(document.getElementById( field_id ));
             
             // The hidden field that will get sent with the form
             $('<input type="hidden"/>').attr({
                 'name': $field.attr('name'),
                 'id': $field.attr('id'),
-                'class': $field.attr('class')
-            }).replaceAll($field)
+                'class': $field.attr('class'),
+                'disabled': $field.attr('disabled')
+            }).replaceAll( $field )
                 // Fill starting value with placeholder 
                 // for later comparisons
-                .val(value);
+                .val( value !== place ? value : '' );
             
         });
         return $(fields);
     };
-
-    // Calls for the necessary adjustments
-    // when a field is changed
+    
+    /**
+    *   Calls for the necessary adjustments when
+    *   a field is changed
+    *   @private
+    */
     function letterChange() {
         var $field = $(this),
             fv     = $field.val();
@@ -198,20 +231,20 @@
         if (fv.length > $field.data('value').length) {
             
             // Apply fieldChance as normal
-            $field.data('value', fieldChange($field));
+            $field.data('value', fieldChange( $field ));
 
         } else if (fv.length < $field.data('value').length) {
             
             // Clear the timeout for the last character
-            window.clearTimeout(last);
+            window.clearTimeout( last );
 
-            var hidden = $field.prev('input'),
-                old    = hidden.val(),
+            var $hidden = $field.prev('input'),
+                old     = $hidden.val(),
                 newVal;
 
-            if (fv.length < old.length - 1) newVal = old.substr(0, fv.length);
+            if ( fv.length < old.length - 1 ) newVal = old.substr( 0, fv.length );
             else {
-                var cp = getCursorPosition($field[0]);
+                var cp = getCursorPosition( $field[0] );
                 
                 // Create the new value with the correct
                 // character deleted
@@ -221,22 +254,29 @@
             }
             
             // Update data and the hidden input
-            $field.data('value', fv).prev('input').val(newVal);
+            $field.data({ 'value': fv, 'newVal': newVal });
+            if ( newVal !== $field.data('placeholder') ) {
+                $field.prev('input').val( newVal );
+            }
         }
     }
-
-    // Updates the field with the dots as the user types,
-    // sets the timeout for the last char,
-    // and returns the new value to set to field.data
-    function fieldChange($field) {
+    
+    /**
+    *   Updates the field with the dots as the user types,
+    *   and sets the timeout for the last char.
+    *   @param {object} $field The field that has changed.
+    *   @return {string} The new value to set to field.data
+    *   @private
+    */
+    function fieldChange( $field ) {
     
         // Clear the timeout for the last character
-        window.clearTimeout(last);
+        window.clearTimeout( last );
         var fv               = $field.val(),
             len              = fv.length,
-            hidden           = $field.prev('input'),
-            old              = hidden.val(),
-            cp               = getCursorPosition($field[0]),
+            $hidden          = $field.prev('input'),
+            old              = $hidden.val(),
+            cp               = getCursorPosition( $field[0] ),
             // HTML encode the character
             encodedCharacter = $('<div>'+opts.character+'</div>').text(),
             newVal;
@@ -246,58 +286,64 @@
         newVal = old.length > cp + 1 && cp > -1 
             ? old.substr(0, cp-1) + fv.charAt(cp-1) + old.substr(cp-1)
             : old + fv.charAt(len - 1);
-        hidden.val(newVal);
+        $field.data('newVal', newVal);
+        if ( newVal !== $field.data('placeholder') ) {
+            $hidden.val( newVal );
+        }
         
-        if (len > 1) {
+        if ( len > 1 ) {
         
             // Loop through and change all characters
             for (var i = 0; i < len - 1; i++) {
-                fv = fv.replace(fv.charAt(i), encodedCharacter);
+                fv = fv.replace( fv.charAt(i), encodedCharacter );
             }
             
             // Update the field
-            $field.val(fv);
+            $field.val( fv );
         }
-        if (len > 0) {
+        if ( len > 0 ) {
         
             // Set the timeout for the last character
             last = setTimeout(function () {
-                cp = getCursorPosition($field[0]);
+                cp = getCursorPosition( $field[0] );
                 fv = $field.val();
-                fv = fv.replace(fv.charAt(len - 1), encodedCharacter);
-                $field.val(fv).data('value', fv);
+                fv = fv.replace( fv.charAt(len - 1), encodedCharacter );
+                $field.val( fv ).data('value', fv);
                 
                 // Reset cursor position to what it was if not at end
-                if (cp != len)
-                    setCursorPosition($field[0], cp);
+                if ( cp != len )
+                    setCursorPosition( $field[0], cp );
             }, opts.delay);
         }
         
         // Reset cursor position to what it was if not at end
         if (cp != len)
-            setCursorPosition($field[0], cp);
+            setCursorPosition( $field[0], cp );
         return fv;
     }
     
-    // Placeholder functionality
+    /**
+    *   Placeholder functionality
+    *   @param {object} $fields The necessary fields to add placeholders.
+    */
     function bindPlaceholder ($fields) {
-        $fields.each(function(i) {
-            var $f     = $(this),
-                hidden = $f.prev('input'),
-                place  = $f.data('placeholder');
+        $fields.each(function(i, item) {
+            var $f      = $(item),
+                $hidden = $f.prev('input'),
+                place   = $f.data('placeholder');
 
             if ( place !== undefined ) {
                 $f.focus(function() {
 
                     // Compare the hidden value with the placeholder value
-                    if ( hidden.val() === place ) {
-                        $f.val('').removeClass( opts.placeholderClass );
-                        hidden.val('');
+                    if ( $f.data('newVal') === place ) {
+                        $f.val('').removeClass( opts.placeholderClass ).data('newVal', '');
+                        $hidden.val('');
                     }
                 }).blur(function() {
                     // If it's empty, put the placeholder in as the value
                     if (place !== undefined && $f.val() === '') {
-                        $f.val( place ).addClass( opts.placeholderClass ).prev('input').val( place ).end();
+                        $f.val( place ).addClass( opts.placeholderClass ).data('newVal', place);
 
                         // Mask the placeholder if needed
                         if ( opts.maskInitial )
@@ -311,14 +357,14 @@
     }
     
     // Extend jQuery
-    $.fn.password123 = function (settings) {
+    $.fn.password123 = function ( settings ) {
         
         // Add in settings to options
         opts = jQuery.extend( opts, settings );
         
         // Replace the fields with what we need
         // and store in var fields
-        var $fields = replaceFields(this);
+        var $fields = replaceFields( this );
         
         // Bind textchange to the fields with
         // the letterChange function
@@ -337,4 +383,4 @@
         // selected are no longer there
         return $fields;
     };
-})(jQuery, this, document);
+})(jQuery, this);
